d9d509c0ead897e865846d853c182a0c6900e584
Jitixaa Sutariya <jitixaasutariya@gmail.com>
2025-01-30 02:35:52 +0000

    add new program for ecma script and datatype

---
+++ JavaScript_Basic/Ecma_Script_Datatype.js
@@ -0,0 +1,37 @@
+"use struct"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Jitixa")
+
+let name = "Jitixa"
+let age = 27
+let isLoggedIn = false
+
+
+
+/*
+ number => 2 to power 53
+ biginit
+ string => ""
+ boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+
+object =>
+
+*/
+
+console.log(typeof(age));
+console.log(typeof isLoggedIn);
+
+
+
+console.log(typeof(name))
+
+console.log(typeof undefined)
+console.log(typeof null)
+
