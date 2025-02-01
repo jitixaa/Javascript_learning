e61ee7bc042aa22ca48261748f12372b3842e79b
Jitixaa Sutariya <jitixaasutariya@gmail.com>
2025-02-01 02:34:12 +0000

    update my new program

---
+++ JavaScript_Basic/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33ass"
+// console.log(typeof(score));
+
+
+let numConvertion = Number(score)
+// console.log(typeof numConvertion)
+// console.log(numConvertion)
+
+
+let isLoggin = ""
+let booleanConvertion = Boolean(isLoggin)
+// console.log(booleanConvertion)
+
+let SomeNumber = 33
+
+let stringConvertion = String(SomeNumber)
+// console.log(stringConvertion)
+
+
+
+// ================================Operation===========================================
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
