e35575b1fe2a0fc682ffb3889a6087ced8568e62
Jitixaa Sutariya <jitixaasutariya@gmail.com>
2025-02-02 05:39:08 +0000

    update new changes

---
+++ JavaScript_Basic/date_time.js
@@ -0,0 +1,45 @@
+let myDate = new Date()
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleDateString());
+console.log(typeof(myDate));
+
+//let myNewDate = new Date(2025,0,2)
+//let myNewDate = new Date(2025,0,2,5,7)
+
+// let myNewDate = new Date("2025-02-02")
+
+// let myNewDate = new Date("02-02-2025")
+// // console.log(myNewDate.toDateString());
+
+
+
+// let myTimeStamp = Date.now()
+// // console.log(myTimeStamp);
+
+// // console.log(myNewDate.getTime());
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+let newnewDate = new Date()
+console.log(newnewDate);
+console.log(newnewDate.getDay() + 1);
+console.log(newnewDate.getMonth() + 1);
+
+
+
+
+
+
+
+
+
+
+
+
