5d90912265a9c87e8db874c3736fd3f4959a6d4e
Jitixaa Sutariya <jitixaasutariya@gmail.com>
2025-02-02 05:17:00 +0000

    update num and math program

---
+++ JavaScript_Basic/num_and_math.js
@@ -0,0 +1,50 @@
+const balance = new Number(4000)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const newNumber = 23.8423
+// console.log(`New Number Print ${newNumber.toPrecision(3)}`)
+
+
+// const hunderds = 1000000000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+
+// const Score = 400
+// console.log(Score);
+
+
+
+// ++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++
+
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(9.56));
+// console.log(Math.ceil(5.6));
+// console.log(Math.floor(6.86));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)) +min);
+
+
+
+
+
+
+
+
+
+
+
